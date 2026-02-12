4d77dffbbb32b0cfbe218ec43589b0e99d388eb7
Kevin <kevinarleymontanacortes@gmail.com>
2026-02-12 00:03:57 -0500

    Estructura base

--- Flume.Domain/Entities/User.cs
+++ Flume.Domain/Entities/User.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Flume.Domain.Entities
 {
     internal class User
