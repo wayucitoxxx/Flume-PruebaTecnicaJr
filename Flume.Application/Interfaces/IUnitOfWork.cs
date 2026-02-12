575b8d60d4d5e96689b61901f572cdbc1e3b5a94
Kevin <kevinarleymontanacortes@gmail.com>
2026-02-12 16:38:04 -0500

    Creacion de interfaces y repositorios

---
+++ Flume.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Flume.Application.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IUserRepository User { get; }
+        ITransactionRepository Transaction { get; }
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ Flume.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using Flume.Domain.Entities;
+
+namespace Flume.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllAsync(CancellationToken cancellationToken);
+        Task AddAsync(User user, CancellationToken cancellationToken);
+    }
+}
