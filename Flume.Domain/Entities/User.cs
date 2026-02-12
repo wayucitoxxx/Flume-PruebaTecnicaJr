66ffdd46286cbb4d0f11a81c256c451e1796dcb7
Kevin <kevinarleymontanacortes@gmail.com>
2026-02-12 12:33:01 -0500

    Creacion de Entidades y Auditoria

--- Flume.Domain/Entities/User.cs
+++ Flume.Domain/Entities/User.cs
@@ -1,7 +1,23 @@
 ﻿
+using Flume.Domain.Common;
+using System.ComponentModel.DataAnnotations;
+
 namespace Flume.Domain.Entities
 {
-    internal class User
+    public class User : AuditableEntity
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        [MaxLength(100)]
+        public string Email { get; set; }
+
+        //Navigation Property
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
     }
 }
---
+++ Flume.Domain/Interfaces/IAuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Flume.Domain.Interfaces
+{
+    public interface IAuditableEntity
+    {
+        int CreatedBy { get; set; }
+        DateTime CreatedDate { get; set; }
+        int? LastModifiedBy { get; set; }
+        DateTime LastModifiedDate { get; set; }
+        bool isActive { get; set; }
+    }
+}
